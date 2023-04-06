b4611a2db03004ad3faf4e3763ca02140b40e5ba
epicnoxious <jappreet16042002@gmail.com>
2023-04-07 00:19:57 +0530

    Add img box

--- static/js/app.js
+++ static/js/app.js
@@ -13,53 +13,53 @@ const locomotivescroll = new LocomotiveScroll({
   },
 });
 
-// .............................tile
-// .............................tile
-// .............................tile
-// .............................tile
-// .............................tile
-
-const container = document.getElementById("tile-container");
-function updateGrid() {
-  const tileSize = Math.floor(window.innerWidth * 0.05); // Set tile size to 5% of screen width
-
-  const bodyHeight = document.body.offsetHeight;
-  const scrollBarWidth = window.innerWidth - document.body.clientWidth;
-  const numRows = Math.ceil((bodyHeight + scrollBarWidth) / tileSize);
-  const numCols = Math.ceil(container.offsetWidth / tileSize);
-
-  // THIS CODE IS CREATING AN ISSUE WITH SCROLL
-  const totalHeight = numRows * tileSize;
-  container.style.height = totalHeight + "px";
-
-  const totalWidth = numCols * tileSize;
-  container.style.width = totalWidth + "px";
-  container.style.boxSizing = "border-box";
-  container.style.maxWidth = "100%"; // Set max-width to 100%
-
-  const root = document.documentElement;
-  root.style.setProperty("--tile-size", tileSize + "px");
-
-  container.innerHTML = "";
-
-  for (let i = 0; i < numRows * numCols; i++) {
-    const tile = document.createElement("div");
-    tile.classList.add("tile");
-    container.appendChild(tile);
-  }
-}
-
-updateGrid();
-
-window.addEventListener("resize", () => {
-  updateGrid(); // Call function whenever window is resized
-});
-
 barba.hooks.after(() => {
   locomotivescroll.destroy();
   updateGrid();
   locomotivescroll.init();
 });
+
+// .............................tile
+// .............................tile
+// .............................tile
+// .............................tile
+// .............................tile
+
+// const container = document.getElementById("tile-container");
+// function updateGrid() {
+//   const tileSize = Math.floor(window.innerWidth * 0.05); // Set tile size to 5% of screen width
+
+//   const bodyHeight = document.body.offsetHeight;
+//   const scrollBarWidth = window.innerWidth - document.body.clientWidth;
+//   const numRows = Math.ceil((bodyHeight + scrollBarWidth) / tileSize);
+//   const numCols = Math.ceil(container.offsetWidth / tileSize);
+
+//   // THIS CODE IS CREATING AN ISSUE WITH SCROLL
+//   const totalHeight = numRows * tileSize;
+//   container.style.height = totalHeight + "px";
+
+//   const totalWidth = numCols * tileSize;
+//   container.style.width = totalWidth + "px";
+//   container.style.boxSizing = "border-box";
+//   container.style.maxWidth = "100%"; // Set max-width to 100%
+
+//   const root = document.documentElement;
+//   root.style.setProperty("--tile-size", tileSize + "px");
+
+//   container.innerHTML = "";
+
+//   for (let i = 0; i < numRows * numCols; i++) {
+//     const tile = document.createElement("div");
+//     tile.classList.add("tile");
+//     container.appendChild(tile);
+//   }
+// }
+
+// updateGrid(); // Call the function for the first time
+
+// window.addEventListener("resize", () => {
+//   updateGrid(); // Call function whenever window is resized
+// });
 
 // .............................barba
 // .............................barba
@@ -73,21 +73,19 @@ barba.init({
     {
       async leave(data) {
         const done = this.async();
-        locomotivescroll.scrollTo(0, 0);
-
         pageTransition();
-
+        locomotivescroll.scrollTo(0, 0);
         await delay(1000);
         // window.scroll(0, 0);
         done();
       },
 
       async enter(data) {
-        mainAnimation();
+        // mainAnimation();
       },
 
       async once(data) {
-        mainAnimation();
+        // mainAnimation();
       },
     },
   ],
@@ -114,15 +112,15 @@ pageTransition = () => {
 mainAnimation = () => {
   var timeline = gsap.timeline();
 
-  timeline.from(".container h1, .menu-items li, .logo", {
-    duration: 1,
-    y: 30,
-    opacity: 0,
-    stagger: {
-      amount: 0.4,
-    },
-    delay: 0.8,
-  });
+  // timeline.from(".container h1, .menu-items li, .logo", {
+  //   duration: 1,
+  //   y: 30,
+  //   opacity: 0,
+  //   stagger: {
+  //     amount: 0.4,
+  //   },
+  //   delay: 0.8,
+  // });
 };
 
 delay = (n) => {
