d847d456f77a0001ee7e174becd9324025d99ba2
epicnoxious <jappreet16042002@gmail.com>
2023-06-15 23:11:04 +0530

    responsive nav

--- algocs/static/js/zindex.js
+++ algocs/static/js/zindex.js
@@ -4,25 +4,36 @@ export function incrementZIndex() {
   boxes.forEach((box) => {
     const imgBoxContainer = box.querySelector(".img-box-container");
     const toggleContainer = box.querySelector(".toggle-container");
+    const toggleContainer2 = box.querySelector(".img-content");
+    const navToggleContainer = box.querySelector(".nav-toggle-container");
+    const navToggleTitle = box.querySelector(".nav-title");
+    const navToggleCut = box.querySelector(".nav-cut");
+    const navToggleUl = box.querySelector(".nav-ul");
     const imgBoxImage = box.querySelector(".img");
     const imgBoxMinus = box.querySelector(".img-cut");
-    const toggleContainer2 = box.querySelector(".img-content");
     box.addEventListener("click", function () {
       // Increment the z-index of the clicked img-box
       this.style.zIndex = ++zIndex;
 
       // Change the background color of the toggle-container element for 1 second
       toggleContainer.style.backgroundColor = "rgb(var(--secondary))";
+      // navToggleUl.style.border = "3px solid rgb(var(--secondary))";
+      // navToggleUl.style.borderTop = "none";
       toggleContainer2.style.backgroundColor = "rgb(var(--secondary))";
       setTimeout(() => {
         toggleContainer.style.backgroundColor = "";
         toggleContainer2.style.backgroundColor = "";
-      }, 100);
+        // navToggleUl.style.border = "";
+      }, 200);
     });
     toggleContainer.addEventListener("click", function () {
       box.classList.toggle("img-box-clicked");
       imgBoxContainer.classList.toggle("img-box-container-clicked");
       toggleContainer.classList.toggle("toggle-container-clicked");
+      navToggleContainer.classList.toggle("nav-toggle-container-clicked");
+      navToggleTitle.classList.toggle("nav-title-clicked");
+      navToggleCut.classList.toggle("nav-cut-clicked");
+      navToggleUl.classList.toggle("nav-ul-clicked");
       imgBoxImage.classList.toggle("img-clicked");
       if (imgBoxMinus.textContent === "-") {
         imgBoxMinus.textContent = "+";
