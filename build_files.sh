7054fbd01827a4340ae8f916c87ddb06e456ec32
epicnoxious <jappreet16042002@gmail.com>
2023-06-27 13:33:53 +0530

    alter build file

--- build_files.sh
+++ build_files.sh
@@ -3,8 +3,8 @@ python3.9 -m pip install -r requirements.txt
 
 
 echo "MAKE MIGRATIONS"
-python3.9 manage.py makemigrations blogs
-python3.9 manage.py migrate blogs
+python3.9 manage.py makemigrations webapp
+python3.9 manage.py migrate webapp
 
 echo "COLLECT STATIC"
 python3.9 manage.py collectstatic --noinput --clear
