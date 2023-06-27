fe23b75ebef60ac044b6d314f02678078133282d
epicnoxious <jappreet16042002@gmail.com>
2023-06-27 13:16:25 +0530

    add build files, responsiveness, imgs, content

--- algocs/algocs/settings.py
+++ algocs/algocs/settings.py
@@ -78,13 +78,23 @@ WSGI_APPLICATION = 'algocs.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'dcidfrzw', 
+        'USER': 'dcidfrzw',
+        'PASSWORD': 'hbyVhAcRlBPmRZNM7d7YagDFS6Ab7kbp',
+        'HOST': 'tiny.db.elephantsql.com', 
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
